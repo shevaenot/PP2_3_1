c2889f463ad511c2953e306b925d08243d4f0daa
enoti <enotishe314@gmail.com>
2023-03-27 18:51:57 +0300

    y

--- src/main/java/shevchenko/controller/UserController.java
+++ src/main/java/shevchenko/controller/UserController.java
@@ -2,7 +2,10 @@ package shevchenko.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import shevchenko.model.User;
 import shevchenko.service.UserService;
 
